4629b9a1c9c2adfacb6229bef8de1990c16ce3fb
ishuvam <ishuvam549@gmail.com>
2025-04-03 03:34:21 +0530

    Commit

---
+++ DAY2/day2/app.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class User(BaseModel):
+    username: str
+    password: str
+
+class DataRequest(BaseModel):
+    data: str
+    query: str
+
+valid_token  = "567ss7qw8VHVHvhjhvHVNVjj785dVjhvjhj67GhhEYnjfds"
+
+valid_username = "shuvam@gmail.com"
+valid_password = "12345@Aa"
+
+@app.post("/user")
+async def check_user(user_data: User):
+    if user_data.username == valid_username and user_data.password == valid_password:
+        return{"message":"Login Success.",
+               "token":valid_token}
+    else:
+        return{"message":"Error"}
+    
+@app.post("/data")
+async def income_data(userData: DataRequest):
+    a = userData.data
+    b = userData.query
+    c = " "
+    d = a + c + b
+
+    return{"message":d}
